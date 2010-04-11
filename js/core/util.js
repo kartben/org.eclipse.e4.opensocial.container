b638c2babed6ad91a1e419dc7f7996716eb3ab39
Benjamin Cabé <kartben@gmail.com>
2010-04-11 18:40:46 +0200

    better implmentation of gadgets.util.registerOnLoadHandler()

--- js/core/util.js
+++ js/core/util.js
@@ -3,8 +3,7 @@ var gadgets = gadgets || {};
 gadgets.util = function() {
 	return {
 		registerOnLoadHandler : function(callback) {
-			callback() ;
-			//window.onload = callback;
+			window.onload = callback;
 		},
 	
     	makeEnum : function (values) {
